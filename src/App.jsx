101e6fbf8a734814056395c648eb1e7089cf6c6b
SOUFIANEOULAHCEN <146205310+SOUFIANEOULAHCEN@users.noreply.github.com>
2024-10-17 12:40:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ListProducts from './components/ListProducts'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+          <ListProducts />
+    </div>
+  )
+}
+
+export default App
